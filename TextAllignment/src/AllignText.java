28182fa5f78afde05fd4350b267ffd9fa4b47422
DimitriosMalonas <dimitriosmalonas@outlook.com>
2019-12-02 05:45:45 +0000

    Fetching command line arguments. Added code to read file contents. Added files with content from which we read.

--- TextAllignment/src/AllignText.java
+++ TextAllignment/src/AllignText.java
@@ -1,9 +1,12 @@
 
+
 public class AllignText {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String[] paragraphs = FileUtil.readFile(args[0]); //Read file into String
+		int length = Integer.parseInt(args[1]);
+		System.out.println(length);
 	}
 
 }
