e4e809629ab12b09227f6ef3cfbe9305548ba46c
DimitriosMalonas <dimitriosmalonas@outlook.com>
2019-12-07 03:58:24 +0000

    Added StringTokenizer method

--- TextAllignment/src/AllignText.java
+++ TextAllignment/src/AllignText.java
@@ -1,4 +1,4 @@
-
+import java.util.StringTokenizer;
 
 /**
  * The AlignText class is the application.
@@ -20,71 +20,81 @@
  */
 
 public class AllignText {
-	public static void main(String[] args) {
-		
+	
+	public static void main(String[] args) {	
 		if (args.length != 2) {
 			System.out.println("java AlignText file_name line_length ");
-		} else {
-			String[] paragraphs = FileUtil.readFile(args[0]);
-			//String p = paragraphs.toString();
-
-			int length = Integer.parseInt(args[1]);
-			System.out.println("text " + paragraphs[0] + "\nlength: " + length + "\n");
-			rightAlign(paragraphs[0], length);
-			
 		}
+		AllignText at = new AllignText();
+		String[] paragraphs = FileUtil.readFile(args[0]);
+		int length = Integer.parseInt(args[1]);			
+		at.alignParagraph(paragraphs[0]);
 	}
 	
 	
-	public static void rightAlign(String strArr, int length) {
-
-		int len = strArr.length();
-		int cnt = 0;
-		for (int i = 0; i < len; i++) {
-			if ((i % 80) == 0) {
-				cnt++;
-			}
-		}
-		
-		int last_line_chars = 0;
-		int other_chars = 0;
-		int cnt2 = 0;
-
-		for (int i = 0; i < len; i++) {
-			other_chars++;
-			if (i % 80 == 0) {
-				cnt2++;
-				if (cnt2 == cnt) {
-					break;
-				}
-			}
-		}
-		
-		last_line_chars = len - other_chars;
-		System.out.println(last_line_chars);
-		int diff = 80 - last_line_chars;
-		System.out.println(diff);
-		cnt2 = 0;
-		for (int i = 0; i < len; i++) {
-			if ((i % 80) == 0) {
-				cnt2++;
-				System.out.println();
-				if (cnt2 == cnt) {
-					for (int j = 0; j < diff; j++) {
-						System.out.print(" ");
-					}
-				}
-			}
-			System.out.print(strArr.charAt(i));
-		}
- 
+	public void alignParagraph(String paragraph) {
+		String[] words = tokenizeParagraph(paragraph);
+		int[] wordsLength = calculateWordLengths(words);
+//		int len = strArr.length();
+//		int cnt = 0;
+//		for (int i = 0; i < len; i++) {
+//			if ((i % 80) == 0) {
+//				cnt++;
+//			}
+//		}
+//		
+//		int last_line_chars = 0;
+//		int other_chars = 0;
+//		int cnt2 = 0;
+//
+//		for (int i = 0; i < len; i++) {
+//			other_chars++;
+//			if (i % 80 == 0) {
+//				cnt2++;
+//				if (cnt2 == cnt) {
+//					break;
+//				}
+//			}
+//		}
+//		last_line_chars = len - other_chars;
+//		System.out.println(last_line_chars);
+//		int diff = 80 - last_line_chars;
+//		System.out.println(diff);
+//		cnt2 = 0;
+//		for (int i = 0; i < len; i++) {
+//			if ((i % 80) == 0) {
+//				cnt2++;
+//				System.out.println();
+//				if (cnt2 == cnt) {
+//					for (int j = 0; j < diff; j++) {
+//						System.out.print(" ");
+//					}
+//				}
+//			}
+//			System.out.print(strArr.charAt(i));
+//		}
 	}
 
 
-
-	int calculateSpaces() {
-		return 1;
+	private int[] calculateWordLengths(String[] words) {
+		// TODO Auto-generated method stub
+		return null;
 	}
+
+
+	private String[] tokenizeParagraph(String paragraph) {
+		// TODO Auto-generated method stub
+	   StringTokenizer st = new StringTokenizer(paragraph," ");
+	   String[] words = new String[st.countTokens()];
+	   
+	   int i = 0;
+	   while(st.hasMoreTokens()) {
+		   words[i] = st.nextToken();
+		   i++;
+	   }
+	   return words;
+	}
+
 }
 
 
